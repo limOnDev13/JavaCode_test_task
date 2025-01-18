1dc412cbdee441b402fe10b1b465ed69b9119c33
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-18 18:24:57 +0500

    Create the schema WalletSchema; rename the schema UpdateWalletSchema to OperationSchema; fix the validator validate_operation_type - now, the names of operations are taken from the Wallet Operations class;

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -1,34 +1,53 @@
 """The module responsible for pydantic schemes."""
 
 from typing import Set
+from uuid import UUID
 
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 
-OPERATION_TYPES: Set[str] = {"DEPOSIT", "WITHDRAW"}
+from src.services.wallet_operations import WalletOperations
 
 
-class UpdateWalletSchema(BaseModel):
+class WalletSchema(BaseModel):
+    """The base scheme of the wallet."""
+
+    uuid: UUID = Field(
+        ...,
+        description="Wallet UUID",
+    )
+    amount: int = Field(
+        ...,
+        description="The number of currency units in the wallet."
+        " The value must be non-negative.",
+        ge=0,
+    )
+    model_config = ConfigDict(from_attributes=True)
+
+
+class OperationSchema(BaseModel):
     """The scheme for conducting operations with the wallet."""
 
-    operationType: str = Field(
-        ...,
-        description="The type of operation with the wallet."
-        " The value must be in OPERATION_TYPES.",
-    )
     amount: int = Field(
         ...,
         description="The amount of the currency unit that the operation"
         " is being performed with. The value must be non-negative.",
         ge=0,
     )
+    operationType: str = Field(
+        ...,
+        description="The type of operation with the wallet."
+        " The value must be in OPERATION_TYPES.",
+    )
 
     @field_validator("operationType", mode="after")
     @classmethod
     def validate_operation_type(cls, value: str) -> str:
         """Validate the field operationType."""
-        if value not in OPERATION_TYPES:
+        set_operations: Set[str] = WalletOperations.set_operations()
+
+        if value not in set_operations:
             raise ValueError(
-                f"operationType must be in {OPERATION_TYPES}."
+                f"operationType must be in {set_operations}."
                 f" The received value is {value}"
             )
         return value
