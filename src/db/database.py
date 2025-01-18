9ee7eaf15ea21cf816eeea1c627d05ea4739c496
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-18 21:07:55 +0500

    Add initialization of the asynchronous engine for the database connection pool; create the dependency injection dependency_session to create a session and transfer it to the handlers.

--- src/db/database.py
+++ src/db/database.py
@@ -2,12 +2,34 @@
 
 from logging import getLogger
 
-from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, async_sessionmaker
+from fastapi import Request
 
 from src.config.app_config import Config
 
 logger = getLogger("main_logger.db")
 
-db_config = Config().db
+config = Config()
 
-engine: AsyncEngine = create_async_engine(db_config.url)
+engine: AsyncEngine = create_async_engine(
+    config.db.url,
+    pre_ping=True,
+    pool_size=10,
+    max_overflow=5,
+    timeout=60.0
+)
+Session = async_sessionmaker()
+
+
+async def dependency_session(request: Request):
+    """Create async pool connections and save it in request.state.pool."""
+    async with Session() as session:
+        try:
+            request.state.session = session
+            yield session
+        except Exception as exc:
+            logger.exception(f"Exception when working with a database:\n{str(exc)}")
+            await session.rollback()
+        else:
+            logger.debug(f"The transaction was completed successfully.")
+            await session.commit()
