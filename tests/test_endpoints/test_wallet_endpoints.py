7f52f9c41bfd2624f2c8d39dafae7b425a60cf05
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-19 12:23:25 +0500

    Create the module test_wallet_endpoints.py for testing the endpoints related to the wallet; add test test_post_wallets_uuid_operation_send_incr_wallet

---
+++ tests/test_endpoints/test_wallet_endpoints.py
@@ -0,0 +1,29 @@
+"""A module for testing endpoints related to the wallet."""
+
+from uuid import UUID, uuid4
+
+import pytest
+from httpx import AsyncClient
+
+from src.schemas.schemas import OperationSchema
+
+
+@pytest.mark.asyncio
+async def test_post_wallets_uuid_operation_send_incr_wallet(
+    client: AsyncClient,
+    operation_inc: OperationSchema,
+):
+    """Test sending a wallet augmentation operation to a wallet."""
+    random_uuid: UUID = uuid4()
+
+    # not existing wallet
+    response = await client.post(
+        f"/api/v1/wallets/{random_uuid}/operation", json=operation_inc.model_dump()
+    )
+    assert response.status_code == 201
+
+    # existing wallet
+    response = await client.post(
+        f"/api/v1/wallets/{random_uuid}/operation", json=operation_inc.model_dump()
+    )
+    assert response.status_code == 200
