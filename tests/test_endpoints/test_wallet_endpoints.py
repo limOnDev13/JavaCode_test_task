2382066a09531100b92d273dfa2c359fb56d5afa
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-19 12:58:01 +0500

    Add the test test_post_wallets_operation_send_decr_wallet

--- tests/test_endpoints/test_wallet_endpoints.py
+++ tests/test_endpoints/test_wallet_endpoints.py
@@ -9,7 +9,7 @@ from src.schemas.schemas import OperationSchema
 
 
 @pytest.mark.asyncio
-async def test_post_wallets_uuid_operation_send_incr_wallet(
+async def test_post_wallets_operation_send_incr_wallet(
     client: AsyncClient,
     operation_inc: OperationSchema,
 ):
@@ -18,12 +18,38 @@ async def test_post_wallets_uuid_operation_send_incr_wallet(
 
     # not existing wallet
     response = await client.post(
-        f"/api/v1/wallets/{random_uuid}/operation", json=operation_inc.model_dump()
+        f"/api/v1/wallets/{str(random_uuid)}/operation", json=operation_inc.model_dump()
     )
     assert response.status_code == 201
 
     # existing wallet
     response = await client.post(
-        f"/api/v1/wallets/{random_uuid}/operation", json=operation_inc.model_dump()
+        f"/api/v1/wallets/{str(random_uuid)}/operation", json=operation_inc.model_dump()
     )
     assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_post_wallets_operation_send_decr_wallet(
+    client: AsyncClient, operation_inc: OperationSchema, operation_dec: OperationSchema
+):
+    """Test sending a wallet reduction operation."""
+    random_uuid: UUID = uuid4()
+
+    # not existing wallet
+    response = await client.post(
+        f"/api/v1/wallets/{str(random_uuid)}/operation", json=operation_inc.model_dump()
+    )
+    assert response.status_code == 201
+
+    # existing wallet
+    response = await client.post(
+        f"/api/v1/wallets/{str(random_uuid)}/operation", json=operation_dec.model_dump()
+    )
+    assert response.status_code == 200
+
+    # Trying to make a negative balance
+    response = await client.post(
+        f"/api/v1/wallets/{str(random_uuid)}/operation", json=operation_dec.model_dump()
+    )
+    assert response.status_code == 400
