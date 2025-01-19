2e52e0be2d2d9e84d0b74fb5c6ed7c983d80fbf5
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-19 10:13:05 +0500

    Fix the repository PostgresWalletRepository - now the session object gets dependencies from the injection. This allows you, in principle, not to call the session in the handlers. Minor fixes; fix docstrings

--- src/db/repositories.py
+++ src/db/repositories.py
@@ -2,11 +2,13 @@
 
 import logging
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Optional
+from typing import Annotated, Any, Dict, Optional, Tuple
 from uuid import UUID
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.db.database import dependency_session
 from src.db.models import Wallet
 from src.schemas.schemas import OperationSchema, WalletSchema
 from src.services.wallet_operations import WalletOperations
@@ -18,13 +20,15 @@ class BaseWalletRepository(ABC):
     """The basic wallet repository."""
 
     @abstractmethod
-    async def update_or_create(self, uuid: UUID, new_data: OperationSchema) -> int:
+    async def update_or_create(
+        self, uuid: UUID, new_data: OperationSchema
+    ) -> Tuple[int, bool]:
         """
         Update existing wallet or create a new.
 
         :param uuid: Wallet UUID.
         :param new_data: The operation data.
-        :return: Current wallet volume.
+        :return: Current wallet volume, and True, if volume was update, else - False.
         """
         pass
 
@@ -42,34 +46,32 @@ class PostgresWalletRepository(BaseWalletRepository):
         session (AsyncSession) - session object.
     """
 
-    def __init__(self, session: AsyncSession):
+    def __init__(self, session: Annotated[AsyncSession, Depends(dependency_session)]):
         """Initialize class."""
         self.__session = session
 
-    async def update_or_create(self, uuid: UUID, new_data: OperationSchema) -> int:
+    async def update_or_create(
+        self, uuid: UUID, new_data: OperationSchema
+    ) -> Tuple[int, bool]:
         """Update existing wallet or create a new."""
         wallet: Optional[Wallet] = await self.__session.get(Wallet, uuid)
         new_data_dict: Dict[str, Any] = new_data.model_dump()
         operation: str = new_data_dict.pop("operationType")
 
-        try:
-            if wallet:
-                wallet_operations: WalletOperations = WalletOperations(wallet.amount)
-                wallet.amount = wallet_operations.make_things(
-                    operation, **new_data_dict
-                )
-            else:
-                wallet_operations = WalletOperations()
-                wallet = Wallet(
-                    uuid=uuid,
-                    amount=wallet_operations.make_things(operation, **new_data_dict),
-                )
-                self.__session.add(wallet)
-        except (ValueError, KeyError) as exc:
-            logger.exception(str(exc))
-            raise exc
+        if wallet:
+            wallet_operations: WalletOperations = WalletOperations(wallet.amount)
+            wallet.amount = wallet_operations.make_things(operation, **new_data_dict)
+            was_updated: bool = True
         else:
-            return wallet.amount
+            wallet_operations = WalletOperations()
+            wallet = Wallet(
+                uuid=uuid,
+                amount=wallet_operations.make_things(operation, **new_data_dict),
+            )
+            self.__session.add(wallet)
+            was_updated = False
+
+        return wallet.amount, was_updated
 
     async def get(self, uuid: UUID) -> Optional[WalletSchema]:
         """Get the wallet by uuid if it exists, else None."""
