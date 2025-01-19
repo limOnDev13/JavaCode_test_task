17c1a48ab6e530b84a45b73f46a3ff405a57060d
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-19 18:01:29 +0500

    Add caching in handlers;

--- src/routes/wallet_route.py
+++ src/routes/wallet_route.py
@@ -7,6 +7,8 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.responses import JSONResponse
 
+from src.cache.base_cache import BaseCache
+from src.cache.redis_cache import dependency_redis
 from src.db.repositories import BaseWalletRepository, PostgresWalletRepository
 from src.schemas.schemas import OperationSchema, WalletSchema
 from src.utils.uuid_validating import validate_uuid
@@ -53,15 +55,20 @@ async def update_wallet(
     request: Request,
     operation: OperationSchema,
     wallet_rep: Annotated[BaseWalletRepository, Depends(PostgresWalletRepository)],
+    cache: Annotated[BaseCache, Depends(dependency_redis)],
 ):
     """Update the wallet or create a new."""
     try:
         wallet_uuid: UUID = request.state.uuid  # from global dependency
-        _, was_updated = await wallet_rep.update_or_create(wallet_uuid, operation)
+        wallet_amount, was_updated = await wallet_rep.update_or_create(
+            wallet_uuid, operation
+        )
     except Exception as exc:
         logger.warning(str(exc))
         raise HTTPException(status_code=400, detail=str(exc))
     else:
+        await cache.put(wallet_uuid, wallet_amount)
+
         if was_updated:
             return dict(msg="OK")
         else:
@@ -107,16 +114,27 @@ async def update_wallet(
 async def get_wallet(
     request: Request,
     wallet_rep: Annotated[BaseWalletRepository, Depends(PostgresWalletRepository)],
+    cache: Annotated[BaseCache, Depends(dependency_redis)],
 ):
     """Get the wallet by uuid."""
     wallet_uuid: UUID = request.state.uuid
-    wallet: Optional[WalletSchema] = await wallet_rep.get(wallet_uuid)
-    if not wallet:
-        raise HTTPException(
-            status_code=404,
-            detail={
-                "msg": "Wallet not found.",
-                "input": str(wallet_uuid),
-            },
-        )
+    value_from_cache: Optional[int] = await cache.get(wallet_uuid)
+
+    if value_from_cache is None:
+        logger.debug("UUID was not in cache.")
+        wallet: Optional[WalletSchema] = await wallet_rep.get(wallet_uuid)
+
+        if not wallet:
+            raise HTTPException(
+                status_code=404,
+                detail={
+                    "msg": "Wallet not found.",
+                    "input": str(wallet_uuid),
+                },
+            )
+        await cache.put(wallet_uuid, wallet.amount)
+    else:
+        logger.debug("UUID was in cache.")
+        wallet = WalletSchema(uuid=wallet_uuid, amount=value_from_cache)
+
     return wallet
