f8d2135f4b4c20c905fe3634d58f0d88d53565ff
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-20 19:52:32 +0500

    Minor fixes

--- stress_tests/stress_post.py
+++ stress_tests/stress_post.py
@@ -1,16 +1,16 @@
-"""The module responsible for load testing of sending multiple post requests."""
+"""The module responsible for load testing of sending multiple POST requests."""
 
 import asyncio
 import random
 import time
-from typing import Any, Dict, List, Optional, Set
-from uuid import UUID, uuid4
+from typing import Any, Dict, List, Optional
+from uuid import UUID
 
 import httpx
 from httpx import Response
 
 from src.schemas.schemas import OperationSchema
-from src.services.wallet_operations import WalletOperations
+from stress_tests.utils import create_operations, create_uuids
 
 
 async def post_request_to_server(url: str, data: Dict[str, Any]) -> httpx.Response:
@@ -19,12 +19,12 @@ async def post_request_to_server(url: str, data: Dict[str, Any]) -> httpx.Respon
         return await client.post(url, json=data)
 
 
-async def ordered_requests_to_single_wallet(
+async def ordered_post_requests_to_single_wallet(
     uuid: UUID, num_operation: int
 ) -> List[Response]:
     """Collect consecutive requests to a single wallet."""
     operations: List[OperationSchema] = create_operations(num_operation)
-    url: str = f"http://localhost:8000/api/v1/wallets/{str(uuid)}/operation"
+    url: str = f"http://127.0.0.1:8000/api/v1/wallets/{str(uuid)}/operation"
 
     responses: List[Response] = list()
     for operation in operations:
@@ -34,27 +34,14 @@ async def ordered_requests_to_single_wallet(
     return responses
 
 
-def create_uuids(number: int = 100) -> List[UUID]:
-    """Generate a list of uuids."""
-    return [uuid4() for _ in range(number)]
-
-
-def create_operations(num_operations: int = 1000) -> List[OperationSchema]:
-    """Generate a list of operations to the wallet."""
-    wallet_operations: Set[str] = WalletOperations().set_operations
-    return [
-        OperationSchema(
-            operationType=random.choice(list(wallet_operations)),
-            amount=random.randint(0, 1000),
-        )
-        for _ in range(num_operations)
-    ]
-
-
 async def thousand_post_of_pain(
     num_requests: int = 1000, num_uuids: int = 50, output_file: Optional[str] = None
 ):
     """Send multiple POST requests asynchronously."""
+    msg: str = (
+        f"Number of users: {num_uuids}; total number of requests: {num_requests}\n"
+    )
+
     uuids: List[UUID] = create_uuids(num_uuids)
     list_uuids = [random.choice(uuids) for _ in range(num_requests)]
     dict_uuids = {uuid_: list_uuids.count(uuid_) for uuid_ in uuids}
@@ -62,7 +49,7 @@ async def thousand_post_of_pain(
     start_time = time.time()
 
     tasks = [
-        ordered_requests_to_single_wallet(uuid_, uuid_count)
+        ordered_post_requests_to_single_wallet(uuid_, uuid_count)
         for uuid_, uuid_count in dict_uuids.items()
     ]
 
@@ -75,7 +62,6 @@ async def thousand_post_of_pain(
         responses.extend(item)
 
     success_count: int = 0
-    msg: str = ""
     for i, resp in enumerate(responses):
         status = resp.status_code
         if 200 <= status < 300 or 400 <= status < 500:
@@ -86,7 +72,7 @@ async def thousand_post_of_pain(
 
     msg += f"Time of requests and responses received: {end_time - start_time}\n"
     msg += f"Number of successful requests: {success_count} / {num_requests}\n"
-    average_time: float = round((end_time - start_time) / num_requests, 2)
+    average_time: float = round((end_time - start_time) / num_requests, 5)
     msg += f"Average query execution time: {average_time}\n"
 
     print(msg)
@@ -97,4 +83,9 @@ async def thousand_post_of_pain(
 
 if __name__ == "__main__":
     thousand_pain: int = 1000
-    asyncio.run(thousand_post_of_pain(thousand_pain, 5))
+    num_users: int = 5
+    asyncio.run(
+        thousand_post_of_pain(
+            thousand_pain, num_users, f"stress_post_{num_users}_{thousand_pain}.txt"
+        )
+    )
