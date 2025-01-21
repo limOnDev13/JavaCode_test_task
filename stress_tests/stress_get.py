60451a97cce735e11265e61937940b7ef74033e4
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2025-01-21 12:56:02 +0500

    To print the results from stress tests in the print_results_of_stress_tests function in the utils module; add logging;

--- stress_tests/stress_get.py
+++ stress_tests/stress_get.py
@@ -1,31 +1,39 @@
 """The module responsible for load testing of sending multiple GET requests."""
 
 import asyncio
+import logging.config
 import random
 import time
 from typing import List, Optional
 from uuid import UUID
 
-import httpx
-from httpx import Response
+from httpx import AsyncClient, Response, Timeout
 
+from src.config.log_config import LOG_CONFIG
 from src.schemas.schemas import OperationSchema
 from stress_tests.stress_post import post_request_to_server
 from stress_tests.utils import create_uuids
 
+from .utils import print_results_of_stress_tests
 
-async def get_request_to_server(url: str) -> httpx.Response:
+logging.config.dictConfig(LOG_CONFIG)
+logger = logging.getLogger("main_logger.stress_tests")
+
+
+async def get_request_to_server(url: str) -> Response:
     """Make a GET request."""
-    async with httpx.AsyncClient() as client:
-        return await client.get(url)
+    logger.info("Doing GET requests to %s", url)
+    async with AsyncClient() as client:
+        return await client.get(url, timeout=Timeout(timeout=300))
 
 
 async def ordered_get_requests_to_single_wallet(
     uuid: UUID, num_requests_from_one: int
 ) -> List[Response]:
     """Collect consecutive requests to a single wallet."""
-    url: str = f"http://127.0.0.1:8000/api/v1/wallets/{str(uuid)}"
+    url: str = f"http://127.0.0.1:8080/api/v1/wallets/{str(uuid)}"
 
+    logger.info("Doing GET requests to wallet with uuid %s", str(uuid))
     responses: List[Response] = list()
     for _ in range(num_requests_from_one):
         response = await get_request_to_server(url)
@@ -38,9 +46,16 @@ async def thousand_get_of_pain(
     num_requests: int = 1000, num_uuids: int = 50, output_file: Optional[str] = None
 ):
     """Send multiple GET requests asynchronously."""
+    logger.info(
+        "Start stress test with num requests %d and num wallets %d",
+        num_requests,
+        num_uuids,
+    )
+    logger.info("Generating uuids...")
     uuids: List[UUID] = create_uuids(num_uuids)
+    logger.info("Creating new wallets...")
     post_urls: List[str] = [
-        f"http://127.0.0.1:8000/api/v1/wallets/{str(wallet_uuid)}/operation"
+        f"http://127.0.0.1:8080/api/v1/wallets/{str(wallet_uuid)}/operation"
         for wallet_uuid in uuids
     ]
 
@@ -53,6 +68,9 @@ async def thousand_get_of_pain(
     ]
     post_responses: List[Response] = await asyncio.gather(*tasks_post)
 
+    logger.info("New wallets were created.")
+    logger.info("Start sending requests.")
+
     start_time = time.time()
 
     tasks_get = [
@@ -64,37 +82,32 @@ async def thousand_get_of_pain(
 
     get_responses: List[List[Response]] = await asyncio.gather(*tasks_get)
     end_time = time.time()
+    logger.info("Responses were received.")
 
     responses: List[Response] = post_responses
     for list_responses in get_responses:
         responses.extend(list_responses)
 
-    success_count: int = 0
-    msg: str = ""
-    for i, resp in enumerate(responses):
-        status = resp.status_code
-        if 200 <= status < 300 or 400 <= status < 500:
-            msg += f"{i}: {status}\n"
-            success_count += 1
-        else:
-            msg += f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!{i}: {status}\n"
-
-    msg += f"Time of requests and responses received: {end_time - start_time}\n"
-    msg += f"Number of successful requests: {success_count} / {num_requests}\n"
-    average_time: float = round((end_time - start_time) / num_requests, 2)
-    msg += f"Average query execution time: {average_time}\n"
-
-    print(msg)
-    if output_file:
-        with open(output_file, "w", encoding="utf-8") as output:
-            output.write(msg)
+    print_results_of_stress_tests(
+        # First, num_uuids of wallet creation requests are made,
+        # then num_requests of regular requests.
+        num_requests=num_requests + num_uuids,
+        num_uuids=num_uuids,
+        method="GET",
+        responses=responses,
+        start_time=start_time,
+        end_time=end_time,
+        output_file=output_file,
+    )
 
 
 if __name__ == "__main__":
     thousand_pain: int = 1000
-    num_users: int = 2
+    num_users: int = 1000
     asyncio.run(
         thousand_get_of_pain(
-            thousand_pain, num_users, f"stress_get_{num_users}_{thousand_pain}.txt"
+            thousand_pain,
+            num_users,
+            f"results/get/stress_get_{num_users}_{thousand_pain}.txt",
         )
     )
--- stress_tests/utils.py
+++ stress_tests/utils.py
@@ -1,8 +1,11 @@
 """The module with auxiliary functions."""
 
 import random
-from typing import List, Set
+from typing import List, Literal, Optional, Set
 from uuid import UUID, uuid4
+
+import httpx
+import requests
 
 from src.schemas.schemas import OperationSchema
 from src.services.wallet_operations import WalletOperations
@@ -23,3 +26,38 @@ def create_operations(num_operations: int = 1000) -> List[OperationSchema]:
         )
         for _ in range(num_operations)
     ]
+
+
+def print_results_of_stress_tests(
+    num_requests: int,
+    num_uuids: int,
+    method: Literal["GET", "POST", "RANDOM"],
+    responses: List[requests.Response] | List[httpx.Response],
+    start_time: float,
+    end_time: float,
+    output_file: Optional[str] = None,
+) -> None:
+    """Print results of stress tests in console and in file."""
+    msg: str = (
+        f"Stress tests with method: {method}"
+        f"Number of users: {num_uuids}; total number of requests: {num_requests}\n"
+    )
+
+    success_count: int = 0
+    for i, resp in enumerate(responses):
+        status = resp.status_code
+        if 200 <= status < 300 or 400 <= status < 500:
+            msg += f"{i}: {status}\n"
+            success_count += 1
+        else:
+            msg += f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!{i}: {status}\n"
+
+    msg += f"Time of requests and responses received: {end_time - start_time}\n"
+    msg += f"Number of successful requests: {success_count} / {num_requests}\n"
+    average_time: float = round((end_time - start_time) / num_requests, 5)
+    msg += f"Average query execution time: {average_time}\n"
+
+    print(msg)
+    if output_file:
+        with open(output_file, "w", encoding="utf-8") as output:
+            output.write(msg)
